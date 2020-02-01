02ce4cf7c9b91b54c0545bccfb56eeee8412d416
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-02-01 15:07:37 +0700

    quizz list

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import QuizCreateView
+from .views import QuizCreateView, QuizListView
 
 app_name = 'quiz'
 
 urlpatterns = [
     path('add/', QuizCreateView.as_view(), name='add_quiz'),
+    path('list/', QuizListView.as_view(), name='quiz_list'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.db.models import Count
+
 
 from account.decorators import teacher_required
 from .models import Quiz
@@ -22,3 +24,16 @@ class QuizCreateView(CreateView):
         messages.success(
             self.request, 'The quiz was created with success! Go ahead and add some questions now.')
         return redirect('account:dashboard')
+
+
+@method_decorator([login_required, teacher_required], name='dispatch')
+class QuizListView(ListView):
+    model = Quiz
+    ordering = ('name', )
+    context_object_name = 'quizzes'
+    template_name = 'quiz/quiz_list.html'
+
+    def get_queryset(self):
+        queryset = self.request.user.quizzes \
+            .annotate(questions_count=Count('questions', distinct=True))
+        return queryset
