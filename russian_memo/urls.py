5d49e9fc6d455a39dbe7c3a2d5b2bc4e4a515c7a
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-02-02 21:03:29 +0700

    fetching cards from django

--- russian_memo/urls.py
+++ russian_memo/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import index
+from words.views import cards, fetch_cards
 
 app_name = 'main'
 name_space = 'main'
@@ -11,4 +14,6 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account',)),
     path('quiz/', include('quiz.urls', namespace='quiz',)),
     path('', index,  name='home'),
-]
+    path('api/cards/', cards, name='cards'),
+    path('fetch/', fetch_cards, name='fetch_cards'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- words/views.py
+++ words/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Card
+
+
+def cards(request):
+    queryset = Card.objects.all()
+    queryset = serialize('json', queryset)
+    return HttpResponse(queryset, content_type="application/json")
+
+
+def fetch_cards(request):
+    return render(request, 'games/fetch_test.html')
