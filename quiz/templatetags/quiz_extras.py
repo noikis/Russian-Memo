3d04f31c8748e23ff83d5f2418aaad939d7bd02b
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-02-06 13:44:59 +0700

    "question_result.html" template customised

--- quiz/templatetags/quiz_extras.py
+++ quiz/templatetags/quiz_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from quiz.models import StudentAnswer
 
 register = template.Library()
 
@@ -6,3 +7,15 @@ register = template.Library()
 @register.filter(name='field_type')
 def field_type(field):
     return field.field.widget.__class__.__name__
+
+
+@register.simple_tag
+def marked_answer(user, opt):
+    studentanswer = StudentAnswer.objects.filter(
+        student=user.student, answer=opt)
+    if studentanswer:
+        if opt.is_correct:
+            return 'correct'
+        return 'wrong'
+
+    return ''
