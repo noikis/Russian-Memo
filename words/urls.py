de70fe8da5116be6034dd12d02638c20e4ecda96
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-02-07 18:51:00 +0700

    СardCreateView

--- words/urls.py
+++ words/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DeckCreateView
+from .views import DeckCreateView, CardCreateView
 
 app_name = 'words'
 
 urlpatterns = [
     path('deck/add/', DeckCreateView.as_view(), name='deck_add'),
+    path('card/add/', CardCreateView.as_view(), name='card_add'),
 ]
--- words/views.py
+++ words/views.py
@@ -3,6 +3,7 @@ from django.core.serializers import serialize
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, UpdateView
 
 
@@ -10,6 +11,7 @@ from .models import Card, Deck
 from account.decorators import teacher_required, student_required
 
 
+@method_decorator([login_required], name='dispatch')
 class DeckCreateView(CreateView):
     model = Deck
     fields = ('category', 'color',)
@@ -19,7 +21,20 @@ class DeckCreateView(CreateView):
         deck = form.save(commit=False)
         deck.save()
         messages.success(self.request, "Deck created!")
-        return redirect('dashboard')
+        return redirect('account:dashboard')
+
+
+@method_decorator([login_required], name='dispatch')
+class CardCreateView(CreateView):
+    model = Card
+    fields = '__all__'
+    template_name = 'words/card_add.html'
+
+    def form_valid(self, form):
+        card = form.save(commit=False)
+        card.save()
+        messages.success(self.request, "Card created!")
+        return redirect('account:dashboard')
 
 
 @login_required
