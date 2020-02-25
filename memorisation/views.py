4f4bc32b5c63c2b302dee2ec7d82d2e3989866b6
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-02-26 01:49:17 +0700

    practice cards one after the other

--- memorisation/views.py
+++ memorisation/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, reverse, redirect
+from datetime import date
 
 from .forms import RatingsForm
 from .models import Practice
@@ -9,16 +10,28 @@ from account.decorators import post_required
 
 @login_required
 def next_practice_item(request):
-    practice = Practice.objects.filter(
-        card__deck__student=request.user.student).order_by('next_practice')[0]
-    form = RatingsForm(initial={"id": practice.id})
-    card = practice.card
-    context = {
-        'practice': practice,
-        'card': card,
-        'form': form
-    }
-    return render(request, 'games/flashcards.html', context)
+    student_practice = Practice.objects.filter(
+        card__deck__student=request.user.student).order_by('next_practice')
+
+    practice = student_practice.filter(
+        next_practice__lte=date.today())
+
+    if len(practice) > 0:
+        practice = practice[0]
+        form = RatingsForm(initial={"id": practice.id})
+        card = practice.card
+        context = {
+            'practice': practice,
+            'card': card,
+            'form': form
+        }
+        return render(request, 'games/flashcards.html', context)
+
+    else:
+        context = {
+            'next_practice': student_practice.first().next_practice
+        }
+        return render(request, 'games/flashcards.html', context)
 
 
 @post_required
@@ -30,7 +43,7 @@ def process_rating(request):
                                           pk=int(form.cleaned_data['id']))
         practice_item.set_next_practice(int(form.cleaned_data['rating']))
         practice_item.save()
-        return redirect('account:dashboard')
+        return redirect('memo:flashcards')
 
 
 @post_required
