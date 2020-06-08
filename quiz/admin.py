2ca80f23bd0ae9d8c5668611efc7e4643b94a4d1
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-06-08 13:05:30 +0700

    deleted quizz app

--- quiz/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Question, Quiz, Answer, TakenQuiz, StudentAnswer
-# Register your models here.
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(TakenQuiz)
