26820dcc497ba1fa468b4aa0674f2983ce30b66e
Ezzahid Ilyas <ezzil1997@gmail.com>
2021-12-09 15:33:14 +0700

    fix: update requirements

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from material.admin.sites import site
 
 from .models import User, Student
 
 
-admin.site.register(User)
-admin.site.register(Student)
+# admin.site.register(User)
+# admin.site.register(Student)
 
 
 # site.unregister(Group)
